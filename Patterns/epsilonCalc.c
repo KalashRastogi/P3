2d853a9f4d09c2c52a486476376fdaa66d00e142
KalashRastogi <kalashrastogi4@gmail.com>
2019-06-03 15:38:22 +0530

    epsilon corrected

--- Patterns/epsilonCalc.c
+++ Patterns/epsilonCalc.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(){
-    double epsilon  = __DBL_EPSILON__;
-    printf("%lf",epsilon);
+    double epsilon = 0.5,prev_epsilon;
+    while(epsilon!=0){
+        prev_epsilon = epsilon;
+        epsilon = epsilon/2;
+    }
+    printf("%f",prev_epsilon);
 
 }
